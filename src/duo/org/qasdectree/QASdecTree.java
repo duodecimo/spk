c3e7c164d8eeac601ec3e66c3cdec53c7967ecd1
duodecimo <duodecimo@gmail.com>
2018-11-16 22:23:37 -0200

    evoluindo

--- src/duo/org/qasdectree/QASdecTree.java
+++ src/duo/org/qasdectree/QASdecTree.java
@@ -8,9 +8,13 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import static java.lang.Math.max;
 import static java.lang.Math.pow;
 import static org.apache.spark.sql.functions.col;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,10 +48,22 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  */
 public class QASdecTree implements Serializable {
-    private boolean debugging = true;
-    private long startTime;
+    private final boolean debugging;
+    private final boolean saveDebugging;
+    private DebugLog debugLog;
+    private final long startTime;
+    private final List<String> words;
 
-    public QASdecTree() {
+    private QASdecTree() {
+        debugging = true;
+        saveDebugging = true;
+        try {
+            debugLog = new DebugLog();
+        } catch (IOException e) {
+            debugLog = null;
+            e.printStackTrace();
+        }
+
         startTime = System.currentTimeMillis();
         Logger.getLogger("org").setLevel(Level.ERROR);
         SparkSession session = SparkSession.builder().appName("qasdectree").master("local[*]").getOrCreate();
@@ -57,7 +73,9 @@ public class QASdecTree implements Serializable {
         /*
         Carga do conjunto de mensagens
          */
-        Dataset<Row> messages = dataFrameReader.option("header","true").csv("/extra2/mySpark/javaNetbeans/data/Training.csv");
+        //Dataset<Row> messages = dataFrameReader.option("header", "true").csv("/extra2/mySpark/javaNetbeans/data/Training.csv");
+        Dataset<Row> messages = dataFrameReader.option("header", "true").
+                csv("/extra2/mySpark/javaNetbeans/data/Training.csv").sample(true, 0.05d);
         //Dataset<Row> messages = dataFrameReader.option("header","true").csv("in/teste.csv");
 
         //messages.cache();
@@ -70,39 +88,55 @@ public class QASdecTree implements Serializable {
         /*
         obter uma lista das palavras (são os cabeçalhos sem a primeira coluna, que é a de respostas (Answers)
          */
-        List<String> words = new ArrayList<>(Arrays.asList(headers));
-        words.remove(0);
-        /*
-        vamos garantir que as colunas de palavras tenham tipo de dado inteiro
+        words = new ArrayList<>(Arrays.asList(headers));
+        removeWord(headers[0]);
 
-        messages.cache();
-        int countColumns = 0;
-        for (String c: words) {
-            messages = messages.withColumn(c, messages.col(c).cast("integer"));
-            countColumns++;
-            if(countColumns % 10 == 0) {
-                mydebug("Convertidas " + countColumns + "/" + words.size()  + " colunas tempo decorrido " + elapsedTime());
-            }
+        /*
+        criar a árvore de decisão
+         */
+        Node QASdecisionTree = createNode(messages, headers[0], "root");
+        /*
+        persistir a arvore
+         */
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            objectOutputStream = new ObjectOutputStream(
+                    new FileOutputStream("/extra2/mySpark/javaNetbeans/data/dTree.ser"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        mydebug("Tempo decorrido para tranformar os tipos dos dados: " + elapsedTime());
-         */
-
-        //System.out.println("=== Mostrar o schema das mensagens ===");
-        //messages.printSchema();
-
-        //System.out.println("=== Mostrar a tabela de mensagens ===");
-        //messages.show();
+        try {
+            if (objectOutputStream != null) {
+                objectOutputStream.writeObject(QASdecisionTree);
+                objectOutputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         /*
-        vamos escolher uma resposta para iniciar o processo:
-        a resposta com maior número de ocorrências.
+        Fim!
          */
-        Dataset<Row> groupedMessages = messages.groupBy(headers[0]).count();
-        // groupedMessages.show();
+        mydebug("Tempo decorrido total: " + elapsedTime());
+        session.stop();
+
+    }
+
+    private Node createNode(Dataset<Row> messages, String answersRow, String nodeName) {
+        /*
+        quantidade total de mensagens
+         */
+        double qm = (double) messages.count();
+        mydebug("quantidade total de mensagens: " + qm);
+
+        /*
+        escolher uma resposta para iniciar o processo:
+        a resposta com maior número de ocorrências.
+        */
+        Dataset<Row> groupedMessages = messages.groupBy(answersRow).count();
         String choosenAnswer = groupedMessages
                 .reduce((a, b) -> a.getLong(1) > b.getLong(1) ? a : b).getString(0);
-        System.out.println("Resposta escolhida: " + choosenAnswer);
-
+        mydebug("Resposta escolhida: " + choosenAnswer);
 
         /*
         calculando gini de O (O = conjunto de mensagens e suas respostas)
@@ -117,18 +151,13 @@ public class QASdecTree implements Serializable {
         p palavras
         pa palavra sendo analisada
         _ negativa
+
          */
 
-
-        /*
-        quantidade total de mensagens
-         */
-        double qm = (double) messages.count();
-        mydebug("quantidade total de mensagens: " + qm);
         /*
         dataset apenas com as linhas da resposta escolhida
          */
-        Dataset<Row> remessages = messages.filter(col(headers[0]).equalTo(choosenAnswer));
+        Dataset<Row> remessages = messages.filter(col(answersRow).equalTo(choosenAnswer));
 
         //remessages.cache();
 
@@ -136,18 +165,22 @@ public class QASdecTree implements Serializable {
         quantidade de mensagens com a resposta escolhida
          */
         double qmre = (double) remessages.count();
-        mydebug("quantidade de mensagens com a resposta escolhida: " + qmre);
+        //mydebug("quantidade de mensagens com a resposta escolhida: " + qmre);
         /*
         pcm = taxa de mensagens com a resposta escolhida (C)
          */
         double tmre = qmre / qm;
-        mydebug("taxa de mensagens com a resposta escolhida: " + tmre);
+        //mydebug("taxa de mensagens com a resposta escolhida: " + tmre);
         /*
         pam = taxa de mensagens sem a resposta escolhida (A)
          */
         double tmre_ = (qm - qmre) / qm;
-        mydebug("taxa de mensagens sem a resposta escolhida: " + tmre_);
+        //mydebug("taxa de mensagens sem a resposta escolhida: " + tmre_);
         double giniO = 1 - (pow(tmre, 2) + pow(tmre_, 2));
+        /*
+        gini negativo não faz sentido ...
+         */
+        giniO = max(0, giniO);
         mydebug("GINI de O: " + giniO);
         mydebug("Tempo decorrido para calcular o GINI de O: " + elapsedTime());
 
@@ -157,66 +190,68 @@ public class QASdecTree implements Serializable {
         int wordCount = 0;
         Map<String, Double> wordsGini = new ConcurrentHashMap<>();
         for (String word: words) {
-            mydebug("=========================");
-            mydebug("Palavra sendo avaliada: " + word);
-            mydebug("=========================");
+            //mydebug("=========================");
+            //mydebug("Palavra sendo avaliada: " + word);
+            //mydebug("=========================");
             /*
             quantidade  de mensagens com a palavra sendo avaliada
              */
             double qmpa = (double) messages.filter(col(word).like("1")).count();
-            mydebug("quantidade  de mensagens com a palavra sendo avaliada: " + qmpa);
+            //mydebug("quantidade  de mensagens com a palavra sendo avaliada: " + qmpa);
             /*
             quantidade de mensagens com a resposta escolhida com a palavra avaliada
              */
             double qmepa = (double) remessages.filter(col(word).like("1")).count();
-            mydebug("quantidade  de mensagens com a resposta escolhida e palavra sendo avaliada: " + qmepa);
+            //mydebug("quantidade  de mensagens com a resposta escolhida e palavra sendo avaliada: " + qmepa);
             /*
             taxa de mensagens com a palavra sendo analisada e a resposta escolhida
              */
             double tmpare = qmpa==0.0d ? 0.0d : qmepa/qmpa;
-            mydebug("taxa de mensagens com a palavra sendo analisada e a resposta escolhida: " + tmpare);
+            //mydebug("taxa de mensagens com a palavra sendo analisada e a resposta escolhida: " + tmpare);
             /*
             taxa de mensagens com a palavra sendo analisada e a resposta diferente da escolhida
              */
             double tmpare_ = qmpa == 0.0d ? 0.0d : (qmpa - qmepa)/qmpa;
-            mydebug("taxa de mensagens com a palavra sendo analisada e a resposta diferenta da escolhida: " + tmpare_);
+            //mydebug("taxa de mensagens com a palavra sendo analisada e a resposta diferenta da escolhida: " + tmpare_);
             /*
             gini das mensagens com a palavra sendo avaliada e com a resposta escolhida
              */
             double ginipare = 1 - (pow(tmpare, 2) + pow(tmpare_, 2));
-            mydebug("gini das mensagens com a palavra sendo avaliada e com a resposta escolhida: " + ginipare);
+            //mydebug("gini das mensagens com a palavra sendo avaliada e com a resposta escolhida: " + ginipare);
             /*
             quantidade de mensagens sem a palavra avaliada com a resposta escolhida
              */
             double qmpa_re = qmre - qmepa;
-            mydebug("quantidade de mensagens sem a palavra avaliada com a resposta escolhida: " + qmpa_re);
+            //mydebug("quantidade de mensagens sem a palavra avaliada com a resposta escolhida: " + qmpa_re);
             /*
             taxa de mensagens com a resposta escolhida sem a palavra avaliada
              */
             double tmpa_re = qmpa == 0.0d ? 0.0d : qmpa_re / qmpa;
-            mydebug("taxa de mensagens com a resposta escolhida sem a palavra avaliada: " + tmpa_re);
+            //mydebug("taxa de mensagens com a resposta escolhida sem a palavra avaliada: " + tmpa_re);
             /*
             taxa de mensagens sem a palavra avaliada e resposta diferente da escolhida
              */
             double tmpa_re_ = qmpa == 0.0d ? 0.0d : (qmpa - qmpa_re)/qmpa;
-            mydebug("taxa de mensagens sem a palavra avaliada e resposta diferente da escolhida: " + tmpa_re_);
+            //mydebug("taxa de mensagens sem a palavra avaliada e resposta diferente da escolhida: " + tmpa_re_);
             /*
             gini das mensagens sem a palavra sendo avaliada e com a resposta escolhida
              */
             double ginipa_re =  1 - (pow(tmpa_re, 2) + pow(tmpa_re_, 2));
-            mydebug("gini das mensagens sem a palavra sendo avaliada e com a resposta escolhida: " + ginipa_re);
+            //mydebug("gini das mensagens sem a palavra sendo avaliada e com a resposta escolhida: " + ginipa_re);
             /*
             calculando o gini da palavra
              */
             double ginipa = giniO - ((ginipare * qm == 0.0d ? 0.0d : qmpa / qm) + (ginipa_re * qm == 0.0d ? 0.0d : qmpa_re / qm));
+            /*
+            gini negativo não faz sentido ...
+             */
+            ginipa = max(0, ginipa);
             wordsGini.put(word, ginipa);
-            mydebug("gini da palavra avaliada: " + ginipa);
+            //mydebug("gini da palavra avaliada: " + ginipa);
             wordCount++;
-            mydebug("Tempo decorrido para calcular o GINI da palavra "  + word +
+            //mydebug("Tempo decorrido para calcular o GINI da palavra "  + word +
                     "(" + wordCount + "/" + words.size() + ") : " + elapsedTime());
         }
-        mydebug("==========================================");
-        mydebug("resumo dos gini:");
         String bestGiniWord = "";
         double bestGini = -1.0d;
         for (String key : wordsGini.keySet()) {
@@ -231,22 +266,93 @@ public class QASdecTree implements Serializable {
             }
         }
         mydebug("==========================================");
+        mydebug("No: " + nodeName);
         mydebug("Palavra com melhor gini: " + bestGiniWord + " (gini " + bestGini + ")");
         mydebug("==========================================");
 
+        Node node = new Node(bestGiniWord, null);
         /*
-        Fim!
+        dividir as mensagens entre as que contém a palavra com melhor GINI e as que não.
          */
-        mydebug("Tempo decorrido total: " + elapsedTime());
-        session.stop();
+        Dataset<Row> messagespa = messages.filter(col(bestGiniWord).like("1"));
+        Dataset<Row> messagespa_ = messages.filter(col(bestGiniWord).like("0"));
+        /*
+        remove a palavra deste nó
+         */
+        removeWord(bestGiniWord);
+
+        /*
+        verifica a expansão da árvore
+         */
+        Dataset<Row> groupedMessagesPa = messagespa.groupBy(answersRow).count();
+        /*
+        verifica expansão à esquerda
+         */
+        if(groupedMessagesPa.count() ==1) {
+            /*
+            so existe uma resposta: é uma folha com decisão de resposta
+             */
+            String sans = groupedMessagesPa.reduce((a, b) -> a.getLong(1) > b.getLong(1) ? a : b).getString(0);
+            node.setLeft(new Node(null, sans));
+            mydebug("Folha a esquerda com resposta: " + sans);
+        } else if(groupedMessagesPa.count() <1 || words.size() == 0) {
+            /*
+            não existe resposta ou esgotaram-se as palavras sem atingir uma única resposta (teria sido capturado acima)
+            É uma folha sem decisão de resposta
+             */
+            node.setLeft(new Node(null, null));
+            mydebug("Folha à esquerda sem resposta.");
+        } else {
+            /*
+            a expansão à esquerda continua
+             */
+            node.setLeft(createNode(messagespa, answersRow, nodeName + ".L"));
+        }
+
+        Dataset<Row> groupedMessagesPa_ = messagespa_.groupBy(answersRow).count();
+        /*
+        verifica expansão à direita
+         */
+        if(groupedMessagesPa_.count() ==1) {
+            /*
+            so existe uma resposta: é uma folha com decisão de resposta
+             */
+            String sans = groupedMessagesPa_.reduce((a, b) -> a.getLong(1) > b.getLong(1) ? a : b).getString(0);
+            node.setRight(new Node(null, sans));
+            mydebug("Folha à direita com resposta: " + sans);
+        } else if(groupedMessagesPa_.count() <1 || words.size() == 0) {
+            /*
+            não existe resposta ou esgotaram-se as palavras sem atingir uma única resposta (teria sido capturado acima)
+            É uma folha sem decisão de resposta
+             */
+            node.setRight(new Node(null, null));
+            mydebug("Folha à direita sem resposta.");
+        } else {
+            /*
+            a expansão à direita continua
+             */
+            node.setRight(createNode(messagespa_, answersRow, nodeName + ".R"));
+        }
+        return node;
+
+    }
+
+    private synchronized void removeWord(String s) {
+        words.remove(s);
     }
 
 
-    private boolean mydebug(String s) {
-        if(debugging = true) {
+    private void mydebug(String s) {
+        if(debugging) {
             System.out.println(s);
         }
-        return debugging;
+        if(saveDebugging && debugLog!=null) {
+            try {
+                debugLog.writeMsg(s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private String elapsedTime() {
@@ -255,6 +361,6 @@ public class QASdecTree implements Serializable {
     }
 
     public static void main(String[] args) {
-        QASdecTree qaSdecTree = new QASdecTree();
+        @SuppressWarnings("unused") QASdecTree qaSdecTree = new QASdecTree();
     }
 }
