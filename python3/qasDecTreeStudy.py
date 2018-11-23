524d51df764b48d502e7e99324943241f474a928
duodecimo <duodecimo@gmail.com>
2018-11-22 23:08:03 -0200

    melhorando python

--- python3/qasDecTreeStudy.py
+++ python3/qasDecTreeStudy.py
@@ -1,24 +1,36 @@
 #!/bin/python
 
 import numpy as np
+import os
+import csv
 from timeit import default_timer as timer
 
 start = timer()
-m = np.random.randint(2, size=(17000, 3000))
+#m = np.random.randint(2, size=(17000, 3000))
 #m = np.random.randint(2, size=(5, 3))
-print('m:')
-print(m)
-s = (m==1).sum(axis=0)
-print('sum: (where value = 1)')
+
+#ler o arquivo csv
+mensagens = []
+with open("../../data/Training.csv", encoding='iso-8859-1') as csvfile:
+    reader = csv.reader(csvfile)
+    palavras = next(reader, None)
+    for mensagem in reader:
+        mensagens.append(mensagem)
+mensagens = np.asarray(mensagens, dtype = np.dtype('uint32'))
+
+print('Arquivo csv:')
+print('mensagens: ', len(mensagens))
+print('palavras: ', len(palavras))
+
+s = (mensagens[:,1:]==1).sum(axis=0)
+print('soma: (quando valor = 1)')
 print(s[:20])
-indexes, values = np.nonzero(m[:1])
-#x = np.array([[1, 2, 3], [1, 5, 6], [2,8,9], [1,11,12], [2,13,14], [3,15,16], [1, 3, 2]], np.int32)
-#print('x:')
-#print(x)
-mi, mc = np.unique(m[:,0], return_counts=True)
-mr = np.column_stack((mi,mc))
-print('mr: (group)')
-print(mr)
+print(mensagens[:5, :5])
+
+respostas, contagem = np.unique(mensagens[:,0], return_counts=True)
+respostas_agrupadas = np.column_stack((respostas,contagem))
+print('respostas_agrupadas: (Respostas agrupadas)')
+print(respostas_agrupadas[:, :])
 
 end = timer()
 print('time running: ', end - start) # Time in seconds, e.g. 5.3809195240028
